86088acf43c13752b3cabee806de7c4c8dd8c87a
Carlos Junior <carlostrader09@gmail.com>
2020-04-12 13:37:04 +0100

    aula1

---
+++ scripts.js
@@ -0,0 +1,16 @@
+qualMes = prompt("Digite um número para saber o mês");
+
+if(qualMes==1){meses.innerHTML="Janeiro"}
+if(qualMes==2){meses.innerHTML="Fevereiro"}
+if(qualMes==3){meses.innerHTML="Marco"}
+if(qualMes==4){meses.innerHTML="Abril"}
+if(qualMes==5){meses.innerHTML="Maio"}
+if(qualMes==6){meses.inneerHTML="Junho"}
+if(qualMes==7){meses.innerHTML="Julho"}
+if(qualMes==8){meses.innerHTML="Agosto"}
+if(qualMes==9){meses.innerHTML="Setembro"}
+if(qualMes==10){meses.innerHTML="Outubro"}
+if(qualMes==11){meses.innerHTML="Novembro"}
+if(qualMes==12){meses.innerHTML="Dezembro"}
+
+
